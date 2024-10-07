f129e2dbf785bbaf0abfdb2bb98acaa7b3862108
Isabela <isabelaroso11@gmail.com>
2024-10-07 13:42:37 -0300

    atualizando tela login

--- src/components/TelaLoginBemVindo.jsx
+++ src/components/TelaLoginBemVindo.jsx
@@ -1,7 +1,13 @@
 function TelaLoginBemVindo() {
     return (
         <div class="bemVindoLogin">
-            <h1 class="titlePrincipal">Bem-Vindo</h1>
+            <p class="titlePrincipal">Bem-Vindo!</p>
+            <form class="input__login" action="login">
+                <input type="email" id="email" name="email" placeholder="Email" required/>
+                <input type="password" id="password" name="password" placeholder="Senha" required/>
+                <button class="button__login" type="submit">Login</button>
+            </form>
+            <a class="esqueceu__senha" href="#">Esqueceu a senha?</a>
         </div>
     )
 }
--- src/components/TelaLoginLeft.jsx
+++ src/components/TelaLoginLeft.jsx
@@ -1,6 +1,15 @@
+import logoEmpresa from '../assets/logoSulagro.png'
+import imgTelaLogin from '../assets/imagemMulherDrone.png'
+
 function TelaLoginLeft() {
     return (
-        <div class="left"></div>
+        <div class="left">
+            <img class="logo__empresa" src={logoEmpresa} alt="logo da empresa"/>
+            <p class="titleEmpresa">
+                <span class="cor__1">Sul</span><span class="cor__2">Agro</span>    
+            </p>
+            <img class="imagem__login" src={imgTelaLogin} alt="mluher com um drone"/>
+        </div>
     )
 }
 
